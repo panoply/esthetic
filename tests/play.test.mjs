3ad168fa821f3fa1515df3fe3d784b4d79ddbd72
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-06-25 04:27:53 +0200

    Update play.test.mjs

--- tests/play.test.mjs
+++ tests/play.test.mjs
@@ -1,14 +1,21 @@
 import test from 'ava';
-import { play } from '@liquify/test-utils';
-import * as prettify from '../index.mjs';
-import * as mocks from './mocks/export.mjs';
+import { play, samples } from '@liquify/test-utils';
+import prettify from '@liquify/prettify';
 
-test('PLAY', t => {
+test('PLAY', async t => {
 
-  prettify.markup(mocks.markup.markup_play, {
-    attemptCorrection: true,
-    forceIndent: true,
-    indentSize: 2
+  const { source } = await samples.get('liquid/javascript');
+
+  prettify.format(source, {
+    attemptCorrection: false,
+    indentSize: 2,
+    script: {
+      objectSort: true,
+      objectIndent: 'indent'
+    },
+    style: {
+      sortProperties: true
+    }
   }).then(value => {
 
     return t.log(play('\n\n' + value));
