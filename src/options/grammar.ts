ab11603869b3f5c3f83e57ab9020217f720c64a5
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-07-11 07:45:25 +0200

    expose grammar extendability on api

---
+++ src/options/grammar.ts
@@ -0,0 +1,188 @@
+import { Grammars } from 'types/prettify';
+import { create } from '@utils/native';
+
+export const grammar = (function () {
+
+  const model: Grammars = create(null);
+
+  model.html = create(null);
+  model.script = create(null);
+  model.liquid = create(null);
+
+  /* -------------------------------------------- */
+  /* HTML                                         */
+  /* -------------------------------------------- */
+
+  model.html.tags = new Set(
+    [
+      'body',
+      'colgroup',
+      'dd',
+      'dt',
+      'head',
+      'html',
+      'li',
+      'option',
+      'p',
+      'tbody',
+      'td',
+      'tfoot',
+      'th',
+      'thead',
+      'tr'
+    ]
+  );
+
+  model.html.voids = new Set(
+    [
+      'area',
+      'base',
+      'basefont',
+      'br',
+      'col',
+      'embed',
+      'eventsource',
+      'frame',
+      'hr',
+      'image',
+      'img',
+      'input',
+      'isindex',
+      'keygen',
+      'link',
+      'meta',
+      'path',
+      'param',
+      'progress',
+      'source',
+      'wbr',
+      'use'
+    ]
+  );
+
+  /* -------------------------------------------- */
+  /* LIQUID                                       */
+  /* -------------------------------------------- */
+
+  model.liquid.tags = new Set(
+    [
+      'case',
+      'capture',
+      'comment',
+      'embed',
+      'filter',
+      'for',
+      'form',
+      'if',
+      'macro',
+      'paginate',
+      'raw',
+      'switch',
+      'tablerow',
+      'unless',
+      'verbatim',
+      'schema',
+      'style',
+      'javascript',
+      'highlight',
+      'stylesheet'
+    ]
+  );
+
+  model.liquid.else = new Set(
+    [
+      'case',
+      'default',
+      'else',
+      'when',
+      'elsif'
+    ]
+  );
+
+  model.liquid.singletons = new Set(
+    [
+      'assign',
+      'increment',
+      'decrement',
+      'render',
+      'include'
+    ]
+  );
+
+  /* -------------------------------------------- */
+  /* SCRIPT                                       */
+  /* -------------------------------------------- */
+
+  model.script.keywords = new Set(
+    [
+      'ActiveXObject',
+      'ArrayBuffer',
+      'AudioContext',
+      'Canvas',
+      'CustomAnimation',
+      'DOMParser',
+      'DataView',
+      'Date',
+      'Error',
+      'EvalError',
+      'FadeAnimation',
+      'FileReader',
+      'Flash',
+      'Float32Array',
+      'Float64Array',
+      'FormField',
+      'Frame',
+      'Generator',
+      'HotKey',
+      'Image',
+      'Iterator',
+      'Intl',
+      'Int16Array',
+      'Int32Array',
+      'Int8Array',
+      'InternalError',
+      'Loader',
+      'Map',
+      'MenuItem',
+      'MoveAnimation',
+      'Notification',
+      'ParallelArray',
+      'Point',
+      'Promise',
+      'Proxy',
+      'RangeError',
+      'Rectangle',
+      'ReferenceError',
+      'Reflect',
+      'RegExp',
+      'ResizeAnimation',
+      'RotateAnimation',
+      'Set',
+      'SQLite',
+      'ScrollBar',
+      'Set',
+      'Shadow',
+      'StopIteration',
+      'Symbol',
+      'SyntaxError',
+      'Text',
+      'TextArea',
+      'Timer',
+      'TypeError',
+      'URL',
+      'Uint16Array',
+      'Uint32Array',
+      'Uint8Array',
+      'Uint8ClampedArray',
+      'URIError',
+      'WeakMap',
+      'WeakSet',
+      'Web',
+      'Window',
+      'XMLHttpRequest'
+    ]
+  );
+
+  return model;
+
+})();
