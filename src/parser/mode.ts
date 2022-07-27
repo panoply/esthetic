dee9dc860cc4bb42bdee00fd6b70b0d94d3b9d15
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-07-27 06:13:29 +0200

    refine runtime mode

--- src/parser/mode.ts
+++ src/parser/mode.ts
@@ -1,67 +1,29 @@
-import type { Prettify } from 'types/prettify';
+import type { Prettify, LanguageProperName } from 'types/prettify';
 import { comment } from '@parser/comment';
 import { parse } from '@parser/parse';
-import { size } from '@utils/helpers';
-import { create, keys } from '@utils/native';
-import * as language from '@parser/language';
+import { size, repeatChar } from '@utils/helpers';
+import { keys, nil } from '@utils/native';
+import { detect, reference } from '@parser/language';
 
+/**
+ * Parse Input
+ *
+ * This a a little wrapper function which will invoke the
+ * lexer and construct the data structure we will use in
+ * the beautify process.
+ */
 export function parser (prettify: Prettify) {
 
-  const langname = prettify.options.language;
-  const langlexer = prettify.options.lexer;
+  const { source, options } = prettify;
+  const { lexer, language } = options;
 
-  parse.count = -1;
-  parse.linesSpace = 0;
-  parse.lineNumber = 1;
-  parse.data.begin = [];
-  parse.data.ender = [];
-  parse.data.lexer = [];
-  parse.data.lines = [];
-  parse.data.stack = [];
-  parse.data.token = [];
-  parse.data.types = [];
-  parse.datanames = [ 'begin', 'ender', 'lexer', 'lines', 'stack', 'token', 'types' ];
-  parse.structure = [ [ 'global', -1 ] ];
-  parse.structure.pop = function pop () {
-    const len = parse.structure.length - 1;
-    const arr = parse.structure[len];
-    if (len > 0) parse.structure.splice(len, 1);
-    return arr;
-  };
-
-  if (prettify.options.language === 'auto' || prettify.options.lexer === 'auto') {
-    const lang = language.detect(prettify.source);
-    if (prettify.options.language === 'auto') prettify.options.language = lang.language;
-    if (prettify.options.lexer === 'auto') prettify.options.lexer = lang.lexer;
-  }
-
-  if (typeof prettify.lexers[prettify.options.lexer] === 'function') {
-
-    // reset references data if store is used on multiple files
-    parse.references = [ [] ];
-    parse.error = '';
-
-    // assign(prettify.options.lexer, prettify.options.lexer);
-
-    for (const lexer of keys(prettify.lexers)) {
-      if (prettify.options.language === 'json') {
-        prettify.options.json = prettify.options.json || create(null);
-      } else {
-        prettify.options[lexer] = prettify.options[lexer] || create(null);
-      }
-    }
-
-    // This line parses the code using a lexer file
-    prettify.lexers[prettify.options.lexer](`${prettify.source} `);
-
+  if (typeof prettify.lexers[lexer] === 'function') {
+    prettify.lexers[lexer](`${source} `);
   } else {
-
-    // restore language and lexer values
-    parse.error = `Specified lexer, ${prettify.options.lexer}, is not a function.`;
+    parse.error = `Specified lexer, ${lexer}, is not a function.`;
   }
 
-  // validate that all the data arrays are the same length
-
+  // Validate that all the data arrays are the same length
   let a = 0;
   let b = 0;
 
@@ -73,140 +35,174 @@ export function parser (prettify: Prettify) {
     b = a + 1;
 
     do {
+
       if (parse.data[k[a]].length !== parse.data[k[b]].length) {
         parse.error = `"${k[a]}" array is of different length than "${k[b]}"`;
         break;
       }
 
       b = b + 1;
+
     } while (b < c);
 
     a = a + 1;
+
   } while (a < c - 1);
 
   // Fix begin values.
   // They must be reconsidered after reordering from object sort
   if (parse.data.begin.length > 0) {
-
-    if (prettify.options.lexer === 'script' && prettify.options[prettify.options.lexer].objectSort) {
+    if (lexer === 'script' && ((
+      language === 'json' &&
+      options.json.objectSort === true
+    ) || (
+      options.language !== 'json' &&
+      options.script.objectSort === true
+    ))) {
       parse.sortCorrection(0, parse.count + 1);
-    } else if (prettify.options.lexer === 'style' && prettify.options[prettify.options.lexer].sortProperties) {
+    } else if (lexer === 'style' && options.style.sortProperties) {
       parse.sortCorrection(0, parse.count + 1);
     }
   }
-
-  prettify.options.language = langname;
-  prettify.options.lexer = langlexer;
 
   return parse.data;
 
 }
 
 /**
- * Mode
+ * Stats Information
+ *
+ * Wrapper for execution statistics available on
+ * the export `format.stats`. Timer starts as soon
+ * as the function is invoked.
+ */
+function stats (language: LanguageProperName) {
+
+  const start: number = Date.now();
+  const store: Partial<Prettify['stats']> = { language, chars: -1 };
+
+  return (output: number): Prettify['stats'] => {
+
+    store.chars = output;
+    store.size = size(output);
+    store.time = (Date.now() - start).toFixed(1) as any;
+
+    return store as Prettify['stats'];
+
+  };
+
+}
+
+/**
+ * Blank Document
+ *
+ * Small function for dealing with blank or empty source strings.
+ * Instead of Prettify passing an empty document to a lexer, we
+ * quickly reason with the input.
+ */
+function blank (prettify: Prettify) {
+
+  const { languageName } = reference(prettify.options.language);
+  const crlf = prettify.options.crlf === true ? '\r\n' : '\n';
+  const input = prettify.source.match(/\n/g);
+  const timer = stats(languageName);
+
+  let output: string = nil;
+
+  if (input === null) {
+
+    if (prettify.options.endNewline) output = crlf;
+
+    prettify.stats = timer(output.length);
+
+  } else {
+
+    output = input[0].length > prettify.options.preserveLine
+      ? repeatChar(prettify.options.preserveLine, crlf)
+      : repeatChar(input[0].length, crlf);
+
+    if (prettify.options.endNewline) output += crlf;
+
+    prettify.stats = timer(output.length);
+
+  }
+
+  return output;
+
+}
+
+/**
+ * Mode Execution
  *
  * This function executes parse and beautification
  * actions.
  */
-export function mode (prettify: Prettify) {
+export function execute (prettify: Prettify) {
 
-  const start = Date.now();
+  prettify.data = parse.init();
 
-  /**
-   * Mode Value, ie: `beautify` or `parse`
-   */
-  const mv = prettify.mode;
-  const lf = prettify.options.crlf === true ? '\r\n' : '\n';
-
-  let result: string = '';
-
-  // Skip formatting when empty document
-  if (prettify.source.trimStart() === '') {
-
-    const preserve = new RegExp(`\\n{${prettify.options.preserveLine},}`);
-
-    result = prettify.source;
-    result = prettify.source.replace(preserve, '\n\n');
-    result = prettify.options.endNewline === true
-      ? result.replace(/\s*$/, lf)
-      : result.replace(/\s+$/, '');
-
-    prettify.stats.chars = result.length;
-    prettify.stats.language = prettify.options.languageName;
-    prettify.stats.size = size(result.length);
-    prettify.stats.time = (Date.now() - start).toFixed(1) as any;
-
-    return result;
-
-  }
+  if (!/\S/.test(prettify.source)) return blank(prettify);
 
   if (prettify.options.language === 'text') {
-    prettify.options.language = 'auto';
-  }
-
-  if (prettify.options.lexer === 'text') {
-    prettify.options.lexer = 'auto';
-  }
-
-  if (prettify.options.language === 'text' || prettify.options.lexer === 'text') {
 
     prettify.options.language = 'text';
     prettify.options.languageName = 'Plain Text';
-    prettify.options.lexer = 'text';
+    prettify.options.lexer = 'markup';
 
-  } else if (prettify.options.language === 'auto' && prettify.options.lexer === 'auto') {
+  } else if (prettify.options.language === 'auto' || prettify.options.language === undefined) {
 
-    const lang = language.detect(prettify.source);
+    const { lexer, language, languageName } = detect(prettify.source);
 
-    if (prettify.options.lexer === 'auto') {
-      prettify.options.lexer = lang.lexer;
+    if (language === 'unknown') {
+      console.warn('Prettify: unknown and/or unsupport language');
+      console.info('Prettify: define a support language (fallback is HTML)');
     }
 
-    if (prettify.options.language === 'auto') {
-      prettify.options.language = lang.language;
-      prettify.options.languageName = lang.languageName;
-    }
+    prettify.options.lexer = lexer;
+    prettify.options.language = language;
+    prettify.options.languageName = languageName;
 
   } else {
 
-    prettify.options.lexer = language.setLexer(prettify.options.language);
+    const { lexer, language, languageName } = reference(prettify.options.language);
+
+    if (language === 'unknown') {
+      console.warn(`Prettify: unsupport ${prettify.options.language}`);
+      console.info('Prettify: language is not supported (fallback is HTML)');
+    }
+
+    prettify.options.lexer = lexer;
+    prettify.options.language = language;
+    prettify.options.languageName = languageName;
 
   }
 
-  prettify.stats.language = prettify.options.languageName;
+  const time = stats(prettify.options.languageName);
+  const mode = prettify.mode;
+  const crlf = prettify.options.crlf === true ? '\r\n' : '\n';
 
-  const comm = comment(prettify);
+  let output: string = prettify.source;
 
-  // If comment returns false, skip formatting and return source
-  if (comm === false) {
-    prettify.stats.time = -1;
-    prettify.stats.chars = prettify.source.length;
-    prettify.stats.size = size(prettify.stats.chars);
-
-    return prettify.source;
+  if (comment(prettify) === false) {
+    prettify.stats = time(output.length);
+    return output;
   }
 
-  prettify.parsed = parser(prettify);
+  prettify.data = parser(prettify);
 
-  if (mv === 'parse') {
-    prettify.stats.time = (Date.now() - start).toFixed(1) as any;
-    prettify.stats.chars = prettify.source.length;
-    prettify.stats.size = size(prettify.stats.chars);
-
-    return prettify.parsed;
+  if (mode === 'parse') {
+    prettify.stats = time(output.length);
+    return parse.data;
   }
 
-  result = prettify.beautify[prettify.options.lexer](prettify.options);
-  result = prettify.options.endNewline === true ? result.replace(/\s*$/, lf) : result.replace(/\s+$/, '');
+  output = prettify.beautify[prettify.options.lexer](prettify.options);
+  output = prettify.options.endNewline === true
+    ? output.replace(/\s*$/, crlf)
+    : output.replace(/\s+$/, nil);
 
-  const length = result.length;
-
-  prettify.stats.chars = length;
-  prettify.stats.size = size(length);
-  prettify.stats.time = (Date.now() - start).toFixed(1) as any;
-
+  prettify.stats = time(output.length);
   prettify.end = 0;
   prettify.start = 0;
 
-  return result;
+  return output;
 }
