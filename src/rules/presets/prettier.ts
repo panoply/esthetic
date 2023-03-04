929cc452bd3922ccd9502d2798b6256dd543949e
Nikolas Savvidis <nicos.savidis@gmail.com>
2023-03-05 00:33:12 +0100

    prettier presets

---
+++ src/rules/presets/prettier.ts
@@ -0,0 +1,44 @@
+import { Rules } from 'types';
+
+/**
+ * Prettier Presets
+ *
+ * Rules which replicates the Prettier formatting style.
+ * These also reflect the prettier-liquid plugin.
+ */
+export const prettier: Rules = {
+  preset: 'prettier',
+  language: 'auto',
+  preserveLine: 1,
+  wrap: 80,
+  liquid: {
+    forceLeadArgument: true,
+    ignoreTagList: [ 'javascript' ],
+    indentAttributes: true,
+    lineBreakSeparator: 'after',
+    dedentTagList: [ 'schema' ],
+    quoteConvert: 'double'
+  },
+  markup: {
+    commentNewline: true,
+    delimiterForce: true,
+    forceAttribute: 1,
+    forceLeadAttribute: true,
+    forceIndent: true,
+    ignoreCSS: true,
+    ignoreJSON: false,
+    selfCloseSpace: true,
+    selfCloseSVG: true,
+    stripAttributeLines: true,
+    quoteConvert: 'double'
+  },
+  json: {
+    arrayFormat: 'indent',
+    objectIndent: 'indent'
+  },
+  style: {
+    commentIndent: false,
+    correct: true,
+    quoteConvert: 'double'
+  }
+};
