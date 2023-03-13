e9b92ed80dd0368d6ccef5a995b6e8d141f2d7d6
Nikolas Savvidis <nicos.savidis@gmail.com>
2023-03-13 05:04:58 +0100

    begin new error logging refinements in markup

--- src/parse/errors.ts
+++ src/parse/errors.ts
@@ -1,13 +1,26 @@
 import { parse } from 'parse/parser';
 import { NWL } from 'lexical/chars';
-import { join, getLanguageName } from 'utils';
+import { join } from 'utils';
+import { getLanguageName } from 'rules/language';
 import { ParseError } from 'lexical/errors';
+import { IParseError } from 'types/index';
 
-export function MarkupError (error: ParseError, token: string, start?: number) {
+/**
+ * Markup Errors
+ *
+ * Generates the error string to throw in markup lexing
+ */
+export function MarkupError (error: ParseError, token: string, name?: string, start?: string) {
 
-  const info = message(error, token, parse.stack.entry[0]);
+  const info = message(error, token, name || parse.stack.entry[0]);
+  const err: IParseError = {};
 
-  return join(
+  err.code = error;
+  err.language = getLanguageName(parse.language);
+  err.details = info.details;
+  err.message = info.message;
+
+  parse.error = join(
     `Syntax Error (line ${parse.lineNumber}): ${info.message}`
     , NWL
     , snippet()
@@ -16,6 +29,39 @@ export function MarkupError (error: ParseError, token: string, start?: number) {
     , NWL
     , `Language: ${getLanguageName(parse.language)} `
     , `Location: ${start}:${parse.lineColumn}  ⟷  ${parse.lineNumber}:${parse.lineColumn}`
+    , `Æsthetic: Parse Failed (Code: ${error})`
+  );
+
+}
+
+/**
+ * Markup Errors
+ *
+ * Generates the error string to throw in markup lexing
+ */
+export function CustomError (error: ParseError, context: {
+  token: string;
+  snippet: string;
+  name: string;
+  line: number;
+}) {
+
+  const info = message(error, context.token, context.name);
+  const err: IParseError = {};
+
+  err.code = error;
+  err.language = getLanguageName(parse.language);
+  err.details = info.details;
+  err.message = info.message;
+
+  return join(
+    `Syntax Error (line ${context.line}): ${info.message}`
+    , NWL
+    , context.snippet
+    , NWL
+    , info.details
+    , NWL
+    , `Language: ${getLanguageName(parse.language)} `
     , `Æsthetic: Parse Failed (Code: ${error})`
   );
 
@@ -73,7 +119,7 @@ function snippet () {
 
     const num = `${no + 1}`;
     const prepend = chars - num.length > 0 ? ` \x1b[90m${num} |` : `\x1b[90m${num} |`;
-    const token = input[no];
+    const token = input[no].trim();
 
     if (no > ender) break;
 
@@ -111,48 +157,52 @@ function snippet () {
  *
  * Returns a relating error message based on the provided enum
  */
-function message (code: ParseError, token: string, name: string) {
+function message (code: ParseError, token: string, name: string): {
+  code: ParseError,
+  message: string;
+  details: string;
+} {
 
   return {
     [ParseError.MissingHTMLEndTag]: ({
       code,
-      message: `Missing HTML ${name} end tag`,
+      message: `Missing HTML \`${name}\` end tag`,
       details: join(
-        `The <${name}> tag type has an incomplete HTML syntactic structure resulting in a parse error.`,
-        `To resolve the issue check that you have a closing </${name}> tag. For more information`,
+        `The \`<${name}>\` tag type has an incomplete HTML syntactic structure resulting in a parse error.`,
+        `To resolve the issue check that you have a closing \`</${name}>\` tag. For more information`,
         'see: https://www.w3.org/TR/html5/syntax.html#closing-elements-that-have-implied-end-tags'
       )
     }),
     [ParseError.MissingLiquidEndTag]: ({
       code,
-      message: `Missing Liquid end${name} tag`,
+      message: `Missing Liquid \`end${name}\` tag`,
       details: join(
-        `The Liquid {% ${name} %} is a tag block type which requires an end tag be provided.`,
-        `To resolve the issue check that you have an ending {% end${name} %} tag. For more information`,
+        `The Liquid \`{% ${name} %}\` is a tag block type which requires an end tag be provided.`,
+        `To resolve the issue check that you have an ending \`{% end${name} %}\` tag. For more information`,
         'see: https://shopify.dev/api/liquid/tags'
       )
     }),
     [ParseError.MissingHTMLStartTag]: ({
       code,
-      message: `Missing HTML start ${token} tag`,
+      message: `Missing HTML start \`${token}\` tag`,
       details: join(
-        `The ${token} tag has incorrect placement or an incomplete structure resulting in a parse error.`,
-        `To resolve the issue, you may need to provide a start <${name}> tag type or correct the placement. `
+        `The \`${token}\` tag has incorrect placement or an incomplete structure resulting in a parse error.`,
+        `To resolve the issue, you may need to provide a start \`<${name}>\` tag type or correct the placement. `
       )
     }),
     [ParseError.MissingLiquidStartTag]: ({
       code,
-      message: `Missing Liquid start {% ${name} %} tag`,
+      message: `Missing Liquid start \`{% ${name} %}"\` tag`,
       details: join(
-        `The ${token} tag has incorrect placement or an incomplete structure resulting in a parse error.`,
-        `To resolve the issue, you may need to provide a start {% ${name} %} tag type or correct the placement. `
+        `The \`${token}\` tag has incorrect placement or an incomplete structure resulting in a parse error.`,
+        `To resolve the issue, you may need to provide a start \`{% ${name} %}\` tag type or correct the placement. `
       )
     }),
     [ParseError.MissingHTMLEndingDelimiter]: ({
       code,
       message: 'Missing HTML > delimiter on end tag',
       details: join(
-        `The ${token} tag is missing its closing delimiter resulting in malformed syntax.`,
+        `The \`${token}\` tag is missing its closing delimiter resulting in malformed syntax.`,
         'You can have Esthetic autofix syntax errors like this by setting the markup rule "correct" to true.'
       )
     }),
@@ -161,15 +211,15 @@ function message (code: ParseError, token: string, name: string) {
       message: 'Invalid HTML Comment Attribute',
       details: join(
         'HTML comments are not allowed inside tags, start or end, at all.',
-        `To resolve the issue, remove the comment ${token} or place it above the tag.`,
+        `To resolve the issue, remove the comment \`${token}\` or place it above the tag.`,
         'For more information see: https://html.spec.whatwg.org/multipage/syntax.html#start-tags'
       )
     }),
     [ParseError.InvalidQuotation]: ({
       code,
-      message: `Invalid quotation ${token} character`,
+      message: `Invalid quotation \`${token}\` character`,
       details: join(
-        `Bad quotation character (\u201c, &#x201c) provided on the ${token} tag. Only single (') or double (")`,
+        `Bad quotation character (\u201c, &#x201c) provided on the \`${token}\` tag. Only single (') or double (")`,
         'quotations characters are valid in HTML (markup) languages. For more information see:',
         'https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(double-quoted)-state'
       )
@@ -178,16 +228,15 @@ function message (code: ParseError, token: string, name: string) {
       code,
       message: 'Invalid CDATA Termination Sequence',
       details: join(
-        `The CDATA ${token} bracket state sequence provided is invalid resulting in a parse error.`,
+        `The CDATA \`${token}\` bracket state sequence provided is invalid resulting in a parse error.`,
         'For more information see: https://html.spec.whatwg.org/multipage/parsing.html#cdata-section-bracket-state'
       )
     }),
     [ParseError.InvalidLiquidCharacterSequence]: ({
       code,
-      message: 'Invalid Liquid Character Sequence',
+      message: `Invalid character sequence in \`${name}\` token`,
       details: join(
-        `The ${name} tag has invalid sequence of characters defined.`,
-        `To resolve the issue, fix the ${token} tag character order.`
+        'An invalid sequence of characters defined'
       )
     }),
     [ParseError.UnterminateString]: ({
