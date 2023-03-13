8a1482d25dfdd0b33040b2f0f0c22c0d97e11cbc
Nikolas Savvidis <nicos.savidis@gmail.com>
2023-03-13 05:05:17 +0100

    Update settings.ts

--- src/parse/settings.ts
+++ src/parse/settings.ts
@@ -1,18 +1,18 @@
+import { ISettings } from 'types';
+import { LogLevel } from 'lexical/enum';
 /**
  * Global Settings
  *
  * This export is responsible for handling the
  * global settings for Æsthetic.
  */
-export const settings = new class Settings {
+export const settings = new class Settings implements ISettings {
 
-  /**
-   * Resolve File
-   *
-   * Configuration file resolution
-   */
-  async resolveFile () {
-
-  }
+  public editorConfig = false;
+  public throwErrors = true;
+  public persistRules = true;
+  public logLevel = LogLevel.Standard;
+  public logColors = true;
+  public resolveConfig = 'package.json';
 
 }();
