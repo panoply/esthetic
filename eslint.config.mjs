2db76fbf8ee8a684b9aa0f020b796ed898115eb1
Nikolas Savvidis <nicos@gmx.com>
2025-02-12 13:30:12 +0100

    eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import shared from '@liquify/eslint-config';
+
+export default [
+  {
+    ignores: [
+      '**/dist/*',
+      '**/node_modules/*'
+    ]
+  },
+  ...shared,
+  {
+    rules: {
+
+      'n/no-callback-literal': 'off',
+      'no-lone-blocks': 'off',
+      'no-unmodified-loop-condition': 'off',
+      'no-unused-vars': 'off',
+      'operator-linebreak': [
+        'error',
+        'after',
+        {
+          overrides: {
+            '?': 'before',
+            ':': 'before',
+            '&&': 'after',
+            '||': 'after',
+            '+': 'before'
+          }
+        }
+      ]
+    }
+  }
+];
