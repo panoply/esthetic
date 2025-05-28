8111249cb5e55c5264d202a1e33856105d84bae5
Nikolas Savvidis <nicos@gmx.com>
2025-05-28 09:45:00 +0200

    Use string comparisons
    Better performance nowadays opposed to charCodes

--- src/lexical/codes.ts
+++ src/lexical/codes.ts
@@ -1,10 +1,147 @@
-/* eslint-disable no-unused-vars */
-
 /* -------------------------------------------- */
 /* CHARACTER CODES                              */
 /* -------------------------------------------- */
 
 export const enum cc {
+  /**
+   * `@` – The at character
+   */
+  ATT = '@',
+  /**
+   * `.` – The dot character
+   */
+  DOT = '.',
+  /**
+   * `^` – Upp Character
+   */
+  UPP = '^',
+  /**
+   * `~` – Squiggle Dash
+   */
+  SDH = '~',
+  /**
+   * `#` – Hashtag character
+   */
+  HSH = '#',
+  /**
+   * `(` – Left Parenthesis
+   */
+  LPR = '(',
+  /**
+   * `)` – Right Parenthesis
+   */
+  RPR = ')',
+  /**
+   * `{` – Left Curly Brace
+   */
+  LCB = '{',
+  /**
+   * `|` – Pipe Character
+   */
+  PIP = '|',
+  /**
+   * `}` – Right Curly Brace
+   */
+  RCB = '}',
+  /**
+   * `[` – Left Square Brace
+   */
+  LSB = '[',
+  /**
+   * `]` – Right Square Brace
+   */
+  RSB = ']',
+  /**
+   * `<` – Left Angle Bracket
+   */
+  LAN = '<',
+  /**
+   * `>` – Right Angle Bracket
+   */
+  RAN = '>',
+  /**
+   * `!` – Bang character
+   */
+  BNG = '!',
+  /**
+   * `-` – Dash character
+   */
+  DSH = '-',
+  /**
+   * `%` – Percent character
+   */
+  PER = '%',
+  /**
+   * `=` – Equals character
+   */
+  EQS = '=',
+  /**
+   * `"` – Double Quoted Character
+   */
+  DQO = '"',
+  /**
+   * `'` – Single Quoted Character
+   */
+  SQO = "'",
+  /**
+   * ` - Template Quoted charactets
+   */
+  TQO = '`',
+  /**
+   * ` ` – Whitespace Character
+   */
+  WSP = ' ',
+  /**
+   * `\n` – Newline Character
+   */
+  NWL = '\n',
+  /**
+   * `\r` – Carriage rEturn Character
+   */
+  CAR = '\r',
+  /**
+   * `\` – Backward Slash
+   */
+  BWS = '\\',
+  /**
+   * `/` – Forward Slash
+   */
+  FWS = '/',
+  /**
+   * `$` – Dollar Character
+   */
+  DOL = '$',
+  /**
+   * `?` – Question Mark
+   */
+  QWS = '?',
+  /**
+   * `*` – Asterix
+   */
+  ARS = '*',
+  /**
+   * `:` - Colon
+   */
+  COL = ':',
+  /**
+   * `;` - Semicolon
+   */
+  SEM = ';',
+  /**
+   * `,` - Comma
+   */
+  COM = ',',
+  /**
+   * `+` - Plus
+   */
+  PLS = '+',
+  /**
+   * `&` - And
+   */
+  AND = '&'
+}
+
+export const enum _cc {
   /**
    * `@` – The at character
    */
